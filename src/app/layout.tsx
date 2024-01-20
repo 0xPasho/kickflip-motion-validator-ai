0f201fc0f86e9ade240726428d7c29adbfb66527
Brandon Ag <brandon.aguilarc@gmail.com>
2024-01-20 06:04:39 -0600

    I need to deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,5 @@
 import { cn } from "@/lib/utils";
 import "./globals.css";
-import { Inter as FontSans } from "next/font/google";
-
-export const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
 
 export default function RootLayout({
   children,
@@ -15,12 +9,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
+      <body className={cn("min-h-screen bg-background font-sans antialiased")}>
         {children}
       </body>
     </html>
